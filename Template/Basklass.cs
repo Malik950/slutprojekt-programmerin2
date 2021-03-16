fe89334c949bad93cab8a0a906f7f7d957f656bf
Malik950 <54435682+Malik950@users.noreply.github.com>
2021-03-16 10:57:19 +0100

    Börjat

---
+++ Template/Basklass.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class Basklass
+    {
+        KeyboardState oldKState;
+        public void Update()
+        {
+            KeyboardState kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Right))
+                PixelPos.X += 10;
+            if (kstate.IsKeyDown(Keys.Left))
+                PixelPos.X -= 10;
+        }
+    }
+}
